42a52424deaf2504b710c01f1fbf9cb051e00c5b
MarcelFeo <marcelfeo29@gmail.com>
2026-02-19 19:25:22 -0300

    start project

---
+++ src/main/java/com/example/ApplyHub/ApplyHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApplyHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApplyHub/ApplyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApplyHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
