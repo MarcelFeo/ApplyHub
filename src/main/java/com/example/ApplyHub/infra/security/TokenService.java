90ed2ad36fa2d5127357c456bfc449eec7e2a1db
MarcelFeo <marcelfeo29@gmail.com>
2026-02-19 20:05:14 -0300

    add generate token function

---
+++ src/main/java/com/example/ApplyHub/infra/security/TokenService.java
@@ -0,0 +1,41 @@
+package com.example.ApplyHub.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.example.ApplyHub.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("ApplyHub")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+
+            return token;
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Error while authenticating: " + e);
+        }
+
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
