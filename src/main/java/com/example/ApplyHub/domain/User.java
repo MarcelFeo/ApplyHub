4b3d8461edc1345869857f0d88a345f5c5bfed3d
MarcelFeo <marcelfeo29@gmail.com>
2026-02-19 19:45:40 -0300

    add user entity

---
+++ src/main/java/com/example/ApplyHub/domain/User.java
@@ -0,0 +1,59 @@
+package com.example.ApplyHub.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String username;
+
+    private String phone;
+
+    private String linkedinUrl;
+
+    private String bio;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
