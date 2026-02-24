e586cef41a22f7c047cb62f0a7df149bef938149
MarcelFeo <marcelfeo29@gmail.com>
2026-02-23 21:06:46 -0300

    add Custom User Details Service class

---
+++ src/main/java/com/example/ApplyHub/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.ApplyHub.infra.security;
+
+import com.example.ApplyHub.domain.User;
+import com.example.ApplyHub.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+
+}
